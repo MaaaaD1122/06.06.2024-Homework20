e6d82f6e3226a3efade4799e588079d01f375808
MaaaaD1122 <164404426+MaaaaD1122@users.noreply.github.com>
2024-06-06 23:24:36 +0300

    Add files via upload

---
+++ Homework20.py
@@ -0,0 +1,11 @@
+class Building:
+    def __init__(self):
+        self.total = 0
+
+    def house_1(self):
+        for j in range(1, 41):
+            print('Этаж № ', j)
+
+
+home = Building
+home.house_1(int)
